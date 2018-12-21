0c3c19f94d98bdb153f9f8c21f87d6aad7b60d12
Andy Tan <andyl7an@gmail.com>
2018-12-21 13:05:52 -0800

    initial commit

--- config/website.js
+++ config/website.js
@@ -5,7 +5,7 @@ module.exports = {
 
   siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
   siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-cara.netlify.com', // Domain of your site. No trailing slash!
+  siteUrl: 'https://andytan.me', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
   siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
