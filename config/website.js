aa1e032cd028aef92c401d9eb3977ed07136bb02
Andy Tan <andyl7an@gmail.com>
2018-12-21 13:13:43 -0800

    changes 2

--- config/website.js
+++ config/website.js
@@ -3,8 +3,8 @@ const tailwind = require('../tailwind');
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
+  siteTitle: 'Andy - Gatsby Starter Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Andy', // Alternative Site title for SEO
   siteUrl: 'https://andytan.me', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
